24e2caf70bd0f2bc887815b34bd741833a61d192
SVolkotrub <volkotrub.design@gmail.com>
2022-05-16 23:27:40 +0300

    update (without forecast)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,99 @@
 
 let code = "a687e5ea475e61b3eb2a5486106b4e28";
-let city = "Kyiv";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+let city ; //city by default= "Kyiv"
+let country; //country by default  = "Ukraine"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
 let currentTemperature;
+let currentDate = new Date();
+
+const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekForecast = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// -- current-location
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  console.log(latitude);
+  console.log(longitude);
+    apiUrl = apiUrl + `&lat=${latitude}&lon=${longitude}`;
+    console.log(apiUrl);
+    axios.get(`${apiUrl}&appid=${code}`).then(showTemperature);
+    axios.get(`${apiUrl}&appid=${code}`).then(showCityCountry);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}):${err.message}`);
+}
+
+function currentLocation(event) {
+    event.preventDefault();
+    console.log('enter function currentLocation');
+    navigator.geolocation.getCurrentPosition(showPosition, error);
+}
+let buttonCurrentLocationElement = document.querySelector("#button-location");
+buttonCurrentLocationElement.addEventListener("click", currentLocation);
+
+//---end current location
+
+//-- search
+
+function search(event) {
+    event.preventDefault();
+    console.log('function search is working');
+    let inputCityElement = document.querySelector("#input-search-city");
+    console.log(inputCityElement);
+    if (inputCityElement.value == false) {
+        alert("Empty city is not valid");
+    } else {
+        axios.get(`${apiUrl}&q=${inputCityElement.value}&appid=${code}`).then(showTemperature).catch(function (error) {
+       alert('Unfortunately, we cannot find such a city in our database, please check the correct city name or try another city');});
+        axios.get(`${apiUrl}&q=${inputCityElement.value}&appid=${code}`).then(showCityCountry).catch(function (error) {
+       console.log('Unfortunately, we cannot find such a city in our database, please check the correct city name or try another city'); });
+    }
+
+}
+let buttonSearchElement = document.querySelector("#search-button");
+buttonSearchElement.addEventListener("click", search);
+
+//--end search
+
+
+// --- fill "part with city and time"
+function showCityCountry(response) {
+    console.log('started function showCityCountry');
+    console.log(response);
+    city = response.data.name;
+    console.log(city);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = city;
+    country = response.data.sys.country;
+    let countryElement = document.querySelector("#country");
+    countryElement.innerHTML = country;
+}
+function formatDate(date) {
+    let currentMonth = date.getMonth();
+    let hours = date.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;}
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;}
+    return ` ${month[currentMonth]} ${date.getDate()}, ${hours}:${minutes}`;
+}
+
+let currentTimeElement = document.querySelector("#locale-time");
+currentTimeElement.innerHTML = formatDate(currentDate);
+
+// ---end  fill "part with city and time"
+//--- fill "center part"
+function showDay(date) {    
+    let weekday = date.getDay();
+    return `${week[weekday]}`;
+}
+
+let headerDayElement = document.querySelector(".current-weekDay-0");
+headerDayElement.innerHTML = showDay(currentDate);
 
 function showTemperature(response) {
     console.log(response);
@@ -22,37 +113,10 @@ function showTemperature(response) {
     windElement.innerHTML = wind;
     currentTemperature = temperature;
 }
-axios.get(`${apiUrl}&appid=${code}`).then(showTemperature);
+axios.get(`${apiUrl}&q=Kyiv&appid=${code}`).then(showTemperature);
 
+//---end fill "center part"
 
-let currentDate = new Date();
-
-const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const weekForecast = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-function formatDate(date) {
-    let currentMonth = date.getMonth();
-    let hours = date.getHours();
-    if (hours < 10) {
-    hours = `0${hours}`;}
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-    minutes = `0${minutes}`;}
-    return ` ${month[currentMonth]} ${date.getDate()}, ${hours}:${minutes}`;
-}
-
-let currentTimeElement = document.querySelector("#locale-time");
-currentTimeElement.innerHTML = formatDate(currentDate);
-
-
-function showDay(date) {    
-    let weekday = date.getDay();
-    return `${week[weekday]}`;
-}
-
-let headerDayElement = document.querySelector(".current-weekDay-0");
-headerDayElement.innerHTML = showDay(currentDate);
 
 function editDayForecast(date) {
     let i = date.getDay();
@@ -64,6 +128,7 @@ function editDayForecast(date) {
 }
 editDayForecast(currentDate);
 
+//--- block CONVERT temperature
 function convertToFahrenheit(event) {
     event.preventDefault();
     let currentTempElement = document.querySelector("#current-temperature"); 
@@ -71,33 +136,17 @@ function convertToFahrenheit(event) {
     currentTempElement.innerHTML = `${tempToFahr}`;
     
 }
-
-
 function convertToCelsius(event) {
     event.preventDefault();
     let currentTempElement = document.querySelector("#current-temperature");
     currentTempElement.innerHTML = currentTemperature; 
 }
-
-
 let linkCelsiusElement = document.querySelector("#celsius");
 let linkFahrElement = document.querySelector("#fahrenheit");
 linkCelsiusElement.addEventListener("click", convertToCelsius);
 linkFahrElement.addEventListener("click", convertToFahrenheit);
 
+//---end block CONVERT temperature
 
-function showCity(event) {
-    event.preventDefault();
-    let inputCityElement = document.querySelector("#input-search-city");
-    
-    if (inputCityElement.value == false) {
-        alert("Empty city is not valid");
-    } else {
-        let headerCityElement = document.querySelector("#city");
-        headerCityElement.innerHTML = inputCityElement.value;
-    }
-}
-let searchElement = document.querySelector("#search-form");
-searchElement.addEventListener("submit", showCity);
 
 
